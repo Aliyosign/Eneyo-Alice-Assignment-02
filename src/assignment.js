1d38f30175c9f78876f5abec89eadd3d8a65a976
Alice Eneyo <aliceeneyo@gmail.com>
2022-07-03 18:08:24 +0000

    First JavaScript Assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const numbersOfSum = arrayOfNumbers.reduce((a,b)=> a+b);
+  return numbersOfSum
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for (let i = 0; i <= arrayOfNumbers.length -1; i++) {
+    if (arrayOfNumbers[i] % 2 === 0) {
+      count++
+    }
+  }
+  return count;
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
